c699260b9e45ee300e22eb0b0f0b4d88efc4073e
Agustin <114120160+NitsugaXD@users.noreply.github.com>
2023-12-04 16:53:10 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,9 +6,9 @@ function downloadApk() {
 
     if (isMobile) {
         // Si es un navegador móvil, redirige a la descarga del APK
-        window.location.href = 'ruta/del/tu-aplicacion.apk';
+        window.location.href = 'AsistenciaDuco.apk'; // Ajusta la ruta según tu estructura
     } else {
         // Si es un navegador de escritorio, muestra un mensaje de error
-        alert('La descarga solo está disponible desde dispositivos móviles.');
+        alert('La descarga está disponible solo desde dispositivos móviles.');
     }
 }
